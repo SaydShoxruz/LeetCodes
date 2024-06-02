18778010e88910af0751832d5cf267c870527f39
Shoxruz <saydshox123@gmail.com>
2024-06-02 06:51:48 +0500

    The third leetcode was done

--- LeetcodesGitHub/Program.cs
+++ LeetcodesGitHub/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            
+
+        }
+        static Dictionary<char, int> romanDigits = new()
+        {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 }
+        };
+
+        static int RomanToInt(string s)
+        {
+            var result = 0;
+
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                if (romanDigits[s[i]] < romanDigits[s[i + 1]])
+                    result -= romanDigits[s[i]];
+                else
+                    result += romanDigits[s[i]];
+            }
+
+            return result + romanDigits[s[s.Length - 1]];
         }
         static bool IsAnagram(string s, string t)
         {
